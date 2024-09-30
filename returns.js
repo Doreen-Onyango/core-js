9a103f0dead1cde709ef3ee72f74d8c37b164726
doonyango <datieno001@gmail.com>
2024-09-30 16:29:05 +0300

    feat: implement the returns

---
+++ returns.js
@@ -0,0 +1,9 @@
+let n = 123456
+// let y = "HEllo World!"
+let y = ['hello', 'there', 'how', 'are', 'you']
+const id = (n) => n
+const resId = id(n)
+console.log(resId)
+const getLength = (y) => y.length
+const resGetLength = getLength(y)
+console.log(resGetLength)
