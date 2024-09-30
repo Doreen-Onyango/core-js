4c7d18c38c82ed8d71032285f06e33564aa80242
doonyango <datieno001@gmail.com>
2024-09-30 16:13:21 +0300

    fix: add variables to hold the values

--- more-or-less.js
+++ more-or-less.js
@@ -1,8 +1,10 @@
+let n = 5
+let b = 10
 const more = (n) => n+1
 const resMore = more(n, 1)
 const less = (n) => n-1
 const resLess = less(n, 1)
-const add = (a, b) => a+b
-const resAdd = add(a, b)
-const sub = (x, y) => x-y
-const resSub = sub(x, y)
+const add = (n, b) => n+b
+const resAdd = add(n, b)
+const sub = (n, b) => n-b
+const resSub = sub(n, b)
