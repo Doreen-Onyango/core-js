51154abd767108eadadb37c71c7cc3b3ebeb23d1
doonyango <datieno001@gmail.com>
2024-09-30 16:09:34 +0300

    feat: implement functions

---
+++ more-or-less.js
@@ -0,0 +1,8 @@
+const more = (n) => n+1
+const resMore = more(n, 1)
+const less = (n) => n-1
+const resLess = less(n, 1)
+const add = (a, b) => a+b
+const resAdd = add(a, b)
+const sub = (x, y) => x-y
+const resSub = sub(x, y)
