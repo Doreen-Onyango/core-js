0cd72b16838770e00897895e22d5d86438574c50
doonyango <datieno001@gmail.com>
2024-10-01 16:29:58 +0300

    fix: update absolute function

--- abs.js
+++ abs.js
@@ -2,7 +2,7 @@ function isPositive(num) {
     return (num >= 0) ? true : false;
 }
 function abs(a) {
- return (a >= 0) ? a : -a;
+ return (a > 0) ? a : -a;
 }
 console.log(isPositive(-10))
 console.log(abs(-4))
