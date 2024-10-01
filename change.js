402dd5849d301d39fcbc61b48757d5d7cd22d245
doonyango <datieno001@gmail.com>
2024-10-01 10:30:53 +0300

    fix(change): update the function

--- change.js
+++ change.js
@@ -1,19 +1,19 @@
-// const sourceObject = {
-//     num: 42,
-//     bool: true,
-//     str: 'some text',
-//     log: console.log,
+const sourceObject = {
+    num: 42,
+    bool: true,
+    str: 'some text',
+    log: console.log,
+}
+const get = (bool) => sourceObject.bool;
+// function get(net) {
+//     return sourceObject.net;
 // }
-// const get = (num) => sourceObject[num];
-function get(net) {
-    return sourceObject.net;
-}
 
-// console.log(get(sourceObject.num));
-// const set = (num, value) => sourceObject.num = value;
-function set(value, net) {
-    sourceObject.value = net;
-    return net;
-}
-// console.log(set(sourceObject.num, 32))
+console.log(get(sourceObject.num));
+const set = (num, value) => sourceObject.num = 100;
+// function set(value, net) {
+//     sourceObject.value = net;
+//     return net;
+// }
+console.log(set(sourceObject.num))
 
