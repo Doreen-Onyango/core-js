60521dd5307fbb9b5bec364dfcd90a4fdee0f071
doonyango <datieno001@gmail.com>
2024-10-01 10:08:13 +0300

    fix(change): update the function

--- change.js
+++ change.js
@@ -1,4 +1,4 @@
 const get = (num) => sourceObject.num;
 console.log(get(sourceObject.num))
-const set = (num, value) => sourceObject.num = 33;
-console.log(set(sourceObject.num))
+const set = (num, value) => sourceObject.num = value;
+console.log(set(sourceObject.num, 32))
