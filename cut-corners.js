c9329c4a05c9b9be92748bffe8a42a509c7ba57c
doonyango <datieno001@gmail.com>
2024-10-03 16:42:39 +0300

    fix: update cut corners functions

--- cut-corners.js
+++ cut-corners.js
@@ -52,11 +52,11 @@ function trunc(num) {
     return num < 0 ? -(Math.abs(num) | 0) : (num | 0);
 }
 
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// console.log(nums.map(round))
+// console.log(nums.map(floor))
+// console.log(nums.map(trunc))
+// console.log(nums.map(ceil))
 // [ 4, -4, 3, -3 ]
 // [ 3, -4, 3, -4 ]
 // [ 3, -3, 3, -3 ]
