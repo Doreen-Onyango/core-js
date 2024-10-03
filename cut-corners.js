9a70d1832765086572e9052bcc3309ece77a5782
doonyango <datieno001@gmail.com>
2024-10-03 13:06:17 +0300

    fix: update cut corners functions

--- cut-corners.js
+++ cut-corners.js
@@ -1,25 +1,25 @@
 // Math.round = Math.ceil = Math.floor = Math.trunc = undefined
 
 function round(num) {
-    return num < 0 ? Math.ceil(num - 0.5) : Math.floor(num + 0.5);
+     return num < 0 ? -(Math.abs(num) + 0.5 | 0) : (num + 0.5) | 0;
 }
 
 function ceil(num) {
-     return (num === Math.floor(num)) ? num : Math.floor(num) + 1;
+     return num === (num | 0) ? num : (num < 0 ? num | 0 : (num | 0) + 1);
 }
 
 function floor(num) {
-     return (num < 0 && num !== Math.floor(num)) ? Math.floor(num) : Math.floor(num);
+    return num > 0 ? ((-num) | 0) * -1 : (num | 0);
 }
 
 function trunc(num) {
-      return (num < 0) ? Math.ceil(num) : Math.floor(num);
+      return num < 0 ? -(Math.abs(num) | 0) : (num | 0);
 }
-// const nums = [3.7, -3.7, 3.1, -3.1]
-// console.log(nums.map(round))
-// console.log(nums.map(floor))
-// console.log(nums.map(trunc))
-// console.log(nums.map(ceil))
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))
 // [ 4, -4, 3, -3 ]
 // [ 3, -4, 3, -4 ]
 // [ 3, -3, 3, -3 ]
