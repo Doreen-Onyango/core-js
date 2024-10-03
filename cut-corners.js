3686e160962b0058dfb70b4d52a764abde3f279b
doonyango <datieno001@gmail.com>
2024-10-03 16:41:53 +0300

    fix: update cut corners functions

--- cut-corners.js
+++ cut-corners.js
@@ -1,25 +1,62 @@
 // Math.round = Math.ceil = Math.floor = Math.trunc = undefined
 
+
+function multiply(a, b) {
+    let result = 0;
+    const isNeg = (b < 0);
+    b = Math.abs(b)
+
+    for (let i = 0; i < b; i++) {
+        result += a;
+    }
+    return isNeg? -result : result;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("no division by zero");
+    }
+    let c = 0
+    const isNeg = ((a < 0) !== (b < 0))
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    while (a >= b) {
+        a -= b;
+        c++;
+    }
+    return isNeg? -c : c;
+}
+
+function modulo(a, b) {
+    if (b === 0) {
+        throw new Error("no modulo by zero");
+    }
+      const divResult = divide(a, b);
+    return a - multiply(divResult, b);
+}
+
 function round(num) {
-     return num < 0 ? -(Math.abs(num) + 0.5 | 0) : (num + 0.5) | 0;
+    return num < 0 ? -(Math.abs(num) + 0.5 | 0) : (num + 0.5) | 0;
 }
 
 function ceil(num) {
-     return num === (num | 0) ? num : (num < 0 ? num | 0 : (num | 0) + 1);
+    return num === (num | 0) ? num : (num < 0 ? (num | 0) : (num | 0) + 1);
 }
 
 function floor(num) {
-    return num > 0 ? ((-num) | 0) * -1 : (num | 0);
+    return num > 0 ? (num | 0) : (num | 0) - (modulo(num, 1) !== 0 ? 1 : 0);
 }
 
 function trunc(num) {
-      return num < 0 ? -(Math.abs(num) | 0) : (num | 0);
+    return num < 0 ? -(Math.abs(num) | 0) : (num | 0);
 }
-// const nums = [3.7, -3.7, 3.1, -3.1]
-// console.log(nums.map(round))
-// console.log(nums.map(floor))
-// console.log(nums.map(trunc))
-// console.log(nums.map(ceil))
+
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))
 // [ 4, -4, 3, -3 ]
 // [ 3, -4, 3, -4 ]
 // [ 3, -3, 3, -3 ]
