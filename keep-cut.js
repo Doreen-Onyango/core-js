5eec0b2db2be123510df0f98f874b96d9a05a115
doonyango <datieno001@gmail.com>
2024-10-03 11:38:02 +0300

    feat: implement keep cut using slice

---
+++ keep-cut.js
@@ -0,0 +1,26 @@
+
+function cutFirst(str) {
+    return str.length > 2 ? str.slice(2) : ''; 
+}
+function cutLast(str) {
+    return str.length > 2 ? str.slice(0, -2) : '';
+}
+function cutFirstLast(str) {
+    return str.length >= 4 ? str.slice(2, -2) : '';
+}
+function keepFirst(str) {
+    return str.length > 2 ? str.slice(0, 2) : '';
+}
+function keepLast(str) {
+    return str.length > 2 ? str.slice(-2) : '';
+}
+function keepFirstLast(str) {
+    return str.length > 4 ? str.slice(0, 2) + str.slice(-2) : '';
+
+}
+console.log(cutFirst("hello"))
+console.log(cutLast("helloyou"))
+console.log(cutFirstLast("hellothere"))
+console.log(keepFirst("how"))
+console.log(keepLast("hello"))
+console.log(keepFirstLast("hellothere"))
