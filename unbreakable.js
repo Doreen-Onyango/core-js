198050d31e46e423b32c1bc1f414220f0ef4d23e
doonyango <datieno001@gmail.com>
2024-10-04 12:11:52 +0300

    feat: implement unbreakable functions

---
+++ unbreakable.js
@@ -0,0 +1,39 @@
+// String.prototype.split = undefined
+// String.prototype.match = undefined
+// RegExp.prototype.exec = undefined
+// Array.prototype.join = undefined
+
+function split(str, sep){
+    const result = [];
+    let curStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        const ch = str[i]
+        if (ch === sep) {
+            result.push(curStr);
+            curStr = '';
+        } else {
+            curStr += ch
+        }
+    }
+    if (curStr) {
+        result.push(curStr)
+    }
+    return curStr
+}
+
+function join(arr, sep) {
+    let result = '';
+    for (let i = 0; i < arr.length; i++){
+        result += arr[i]
+        
+        if (i < arr.length-1) {
+            result += sep;
+        }
+    }
+    return result
+}
+let arr = ['hello', "Dorah"]
+let str = "hello Dorah"
+console.log(join(arr, "-"))
+console.log(split(str, ","))
