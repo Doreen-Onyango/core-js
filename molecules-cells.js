f19026ca39675bf0ecb18dc6e3cf157caa6ec0ef
doonyango <datieno001@gmail.com>
2024-10-04 22:24:44 +0300

    feat: implement molecules cells functions

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,47 +1,33 @@
-function DNAtoRNA(dna) {
-    let rna = '';
-    for (let i = 0; i < dna.length; i++) {
-        switch (dna[i]) {
-            case 'G':
-                rna += 'C';
-                break;
-            case 'C':
-                rna += 'G';
-                break;
-            case 'T':
-                rna += 'A';
-                break;
-            case 'A':
-                rna += 'U';
-                break;
-            default:
-                throw new Error('Invalid DNA nucleotide: ' + dna[i]);
+function RNAtoDNA(str) {
+    let newstr = ""
+    for (let i = 0; i <= str.length -1; i++) {
+        if (str[i]=== "G") {
+            newstr += "C"
+        } else if (str[i] === "C") {
+            newstr += "G"
+        } else if (str[i]=== "T") {
+            newstr += "A"
+        } else if (str[i]=== "A") {
+            newstr += "U"
         }
     }
-    return rna;
-}
-function RNAtoDNA(rna) {
-    let dna = '';
-    for (let i = 0; i < rna.length; i++) {
-        switch (rna[i]) {
-            case 'C':
-                dna += 'G';
-                break;
-            case 'G':
-                dna += 'C';
-                break;
-            case 'A':
-                dna += 'T';
-                break;
-            case 'U':
-                dna += 'A';
-                break;
-            default:
-                throw new Error('Invalid RNA nucleotide: ' + rna[i]);
-        }
-    }
-    return dna;
+    return newstr
 }
 
-console.log(DNAtoRNA("ATCG")); 
-console.log(RNAtoDNA("UAGC")); 
+function DNAtoRNA(str) {
+    let newstr = ""
+    for (let i = 0; i <= str.length -1; i++) {
+        if (str[i]=== "G") {
+            newstr += "C"
+        } else if (str[i] === "C") {
+            newstr += "G"
+        } else if (str[i]=== "U") {
+            newstr += "A"
+        } else if (str[i]=== "A") {
+            newstr += "T"
+        }
+    }
+    return newstr
+}
+// console.log(DNAtoRNA("ATCG")); 
+// console.log(RNAtoDNA("UAGC")); 
