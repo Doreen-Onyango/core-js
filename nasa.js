20b1995181d1d1e876e5dca8358c767ccc87690e
doonyango <datieno001@gmail.com>
2024-10-04 16:23:39 +0300

    feat: implement nasa function

---
+++ nasa.js
@@ -0,0 +1,17 @@
+function nasa(N, cur) {
+    if (cur > N) {
+        return '';
+    }
+    let result= "";
+    if (cur % 3 === 0 && cur % 5 === 0) {
+        result = 'NASA';
+    } else if (cur % 3 === 0) {
+        result = 'NA';
+    } else if (cur % 5 === 0) {
+        result = 'SA';
+    } else {
+        result = cur;
+    }
+    return result + (cur < N ? ' ' : '') + nasa(N, cur + 1);
+}
+console.log(nasa(15, 3))
