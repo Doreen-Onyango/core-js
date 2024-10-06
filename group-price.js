9d45c2faa00b01c6fcb8597b6768f5710c8a3f3b
doonyango <datieno001@gmail.com>
2024-10-06 08:57:34 +0300

    feat: implement group price function

---
+++ group-price.js
@@ -0,0 +1,23 @@
+function groupPrice(str) {
+    const priceRegex = /(\$|€|£)?(\d+)(?:[.,](\d{2}))?(?!\d)/g;
+    const results = [];
+    let match;
+  
+    while ((match = priceRegex.exec(str)) !== null) {
+      const [fullMatch, currency = '', dollars, cents] = match;
+      let formattedPrice = '';
+      
+      if (currency) {
+        formattedPrice = `${currency}${dollars}${cents ? '.' + cents : ''}`;
+      } else if (str.substring(match.index - 3, match.index).toUpperCase() === 'USD') {
+        formattedPrice = `USD${dollars}${cents ? '.' + cents : ''}`;
+      } else {
+        continue;
+      }
+      
+      results.push([formattedPrice, dollars, cents || '']);
+    }
+  
+    return results;
+}
+  
