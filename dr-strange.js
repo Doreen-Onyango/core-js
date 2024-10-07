dc5553cc6557ff9e318f68bbd6a0ffefa74668cc
doonyango <datieno001@gmail.com>
2024-10-07 17:25:51 +0300

    feat: implement timetravel function

--- dr-strange.js
+++ dr-strange.js
@@ -1,5 +1,5 @@
 function addWeek(date) {
-    const epoch = new date('0001-01-01');
+    const epoch = new Date('0001-01-01');
     const timeDif = date - epoch;
     const dayFromEpoch = Math.floor(timeDif/(1000 *60*60*24));
     const newDayInd = dayFromEpoch % 14;
@@ -7,6 +7,27 @@ function addWeek(date) {
     return days[newDayInd];
 }
 
-function timeTravel() {
-
+function timeTravel({date, hour, minute, second}) {
+    const newDate = new Date(date);
+    newDate.setHours(hour);
+    newDate.setMinutes(minute);
+    newDate.setSeconds(second);
+    return newDate;
     }
+
+console.log(addWeek(new Date('0001-01-01'))) // Output: Monday
+console.log(addWeek(new Date('0001-01-02'))) // Output: Tuesday
+console.log(addWeek(new Date('0001-01-07'))) // Output: Sunday
+console.log(addWeek(new Date('0001-01-08'))) // Output: secondMonday
+console.log(addWeek(new Date('0001-01-09'))) // Output: secondTuesday
+
+// timeTravel({ date, hour, minute, second })
+
+console.log(timeTravel({
+  date: new Date('2020-05-29 23:25:22'),
+  hour: 21,
+  minute: 22,
+  second: 22,
+}).toString())
+
+// Output: Fri May 29 2020 21:22:22 GMT+0100 (Western European Summer Time)
