011fb464b8eef7ab393c951c39ec6c980e1b9691
doonyango <datieno001@gmail.com>
2024-10-08 05:41:13 +0300

    feat: implement date functions

--- date-is.js
+++ date-is.js
@@ -16,6 +16,12 @@ function isBefore(date1, date2) {
     return date1 < date2;
 }
 
+function isAfter(date1, date2) {
+     if (!isValid(date1) || !isValid(date2)) {
+        return false;
+    } 
+    return date1 > date2;
+}
 function isFuture(date) {
     if (!isValid(date)) {
         return false;
