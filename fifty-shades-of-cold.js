18cf05f3680cfaebc9833bcfdb4167d755419c23
doonyango <datieno001@gmail.com>
2024-10-12 22:40:54 +0300

    feat: implement fifty shades of cold

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,144 +1,37 @@
-export const colors = [
-    'indianred',
-    'lightcoral',
-    'salmon',
-    'darksalmon',
-    'lightsalmon',
-    'crimson',
-    'red',
-    'firebrick',
-    'darkred',
-    'pink',
-    'lightpink',
-    'hotpink',
-    'deeppink',
-    'mediumvioletred',
-    'palevioletred',
-    'orange',
-    'coral',
-    'tomato',
-    'orangered',
-    'darkorange',
-    'yellow',
-    'gold',
-    'lightyellow',
-    'lemonchiffon',
-    'lightgoldenrodyellow',
-    'papayawhip',
-    'moccasin',
-    'peachpuff',
-    'palegoldenrod',
-    'khaki',
-    'darkkhaki',
-    'lavender',
-    'thistle',
-    'plum',
-    'violet',
-    'orchid',
-    'fuchsia',
-    'magenta',
-    'mediumorchid',
-    'mediumpurple',
-    'rebeccapurple',
-    'blueviolet',
-    'darkviolet',
-    'darkorchid',
-    'darkmagenta',
-    'purple',
-    'indigo',
-    'slateblue',
-    'darkslateblue',
-    'green',
-    'greenyellow',
-    'chartreuse',
-    'lawngreen',
-    'lime',
-    'limegreen',
-    'palegreen',
-    'lightgreen',
-    'mediumspringgreen',
-    'springgreen',
-    'mediumseagreen',
-    'seagreen',
-    'forestgreen',
-    'darkgreen',
-    'yellowgreen',
-    'olivedrab',
-    'olive',
-    'darkolivegreen',
-    'mediumaquamarine',
-    'darkseagreen',
-    'lightseagreen',
-    'darkcyan',
-    'teal',
-    'aqua',
-    'cyan',
-    'lightcyan',
-    'paleturquoise',
-    'aquamarine',
-    'turquoise',
-    'mediumturquoise',
-    'darkturquoise',
-    'cadetblue',
-    'steelblue',
-    'lightsteelblue',
-    'powderblue',
-    'lightblue',
-    'skyblue',
-    'lightskyblue',
-    'deepskyblue',
-    'dodgerblue',
-    'cornflowerblue',
-    'mediumslateblue',
-    'royalblue',
-    'blue',
-    'mediumblue',
-    'darkblue',
-    'navy',
-    'midnightblue',
-    'brown',
-    'cornsilk',
-    'blanchedalmond',
-    'bisque',
-    'navajowhite',
-    'wheat',
-    'burlywood',
-    'tan',
-    'rosybrown',
-    'sandybrown',
-    'goldenrod',
-    'darkgoldenrod',
-    'peru',
-    'chocolate',
-    'saddlebrown',
-    'sienna',
-    'maroon',
-    'white',
-    'snow',
-    'honeydew',
-    'mintcream',
-    'azure',
-    'aliceblue',
-    'ghostwhite',
-    'whitesmoke',
-    'seashell',
-    'beige',
-    'oldlace',
-    'floralwhite',
-    'ivory',
-    'antiquewhite',
-    'linen',
-    'lavenderblush',
-    'mistyrose',
-    'gainsboro',
-    'lightgray',
-    'silver',
-    'darkgray',
-    'gray',
-    'dimgray',
-    'lightslategray',
-    'slategray',
-    'darkslategray',
-    'black',
-  ]
+// Assume colors array is provided elsewhere
+const colors = [
+    'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'
+  ];
   
+  export function generateClasses() {
+    const style = document.createElement('style');
+    let styleContent = '';
+    
+    colors.forEach(color => {
+      styleContent += `.${color} { background: ${color}; }\n`;
+    });
+    
+    style.textContent = styleContent;
+    document.head.appendChild(style);
+  }
+  
+  export function generateColdShades() {
+    const coldColors = ['aqua', 'blue', 'turquoise', 'green', 'cyan', 'navy', 'purple'];
+    const container = document.body;
+    
+    colors.forEach(color => {
+      if (coldColors.some(coldColor => color.includes(coldColor))) {
+        const div = document.createElement('div');
+        div.className = color;
+        div.textContent = color;
+        container.appendChild(div);
+      }
+    });
+  }
+  
+  export function choseShade(shade) {
+    const divs = document.querySelectorAll('div');
+    divs.forEach(div => {
+      div.className = shade;
+    });
+  }
