12949765b9d95cd09ded694bb10b0723fde3c359
doonyango <datieno001@gmail.com>
2024-10-16 15:44:03 +0300

    feat: implement curry entries

--- curry-entries.js
+++ curry-entries.js
@@ -38,20 +38,20 @@ function reduceScore(personnel, initialValue = 0) {
     return reduceCurry((acc, [k, v]) => v.isForceUser ? acc + v.shootingScore + v.pilotingScore : acc)(personnel, initialValue);
 }
 
-const personnel = {
-    lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-    sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-    zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-    ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-    calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-  }
+// const personnel = {
+//     lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//     sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//     zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//     ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//     calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+//   }
   
 
-  const personnelWithAverage = mapAverage(personnel);
-  console.log("Personnel with Average Scores:", personnelWithAverage);
+//   const personnelWithAverage = mapAverage(personnel);
+//   console.log("Personnel with Average Scores:", personnelWithAverage);
   
-  const forceUsers = filterForce(personnel);
-  console.log("Force Users with Shooting Score >= 80:", forceUsers);
+//   const forceUsers = filterForce(personnel);
+//   console.log("Force Users with Shooting Score >= 80:", forceUsers);
   
-  const totalScore = reduceScore(personnel);
-  console.log("Total Score for Force Users:", totalScore);
+//   const totalScore = reduceScore(personnel);
+//   console.log("Total Score for Force Users:", totalScore);
