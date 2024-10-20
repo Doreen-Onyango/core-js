f806431703411d5b95b19ce71bf55b61bb35c5ce
doonyango <datieno001@gmail.com>
2024-10-20 05:19:21 +0300

    feat: implement gougleSearch

--- gougle-search.js
+++ gougle-search.js
@@ -1,32 +1,37 @@
-async function race(args) {
-    let newPromise = new Promise((res, rej) => {
-      args.forEach((promise) => {
-        promise.then(res, rej);
-      });
-    });
-    return newPromise;
+async function queryServers(serverName, q) {
+    const queryString = new URLSearchParams({ q }).toString();
+  
+    const urls = [
+      getJSON(`/${serverName}?${queryString}`),
+      getJSON(`/${serverName}_backup?${queryString}`),
+    ];
+  
+    // Use Promise.race to return the fastest result
+    return Promise.race(urls);
   }
   
-  async function some(args, count) {
-    if (!args.length || !count) return Promise.resolve([]);
-    let newArr = new Array(args.length);
-    await new Promise((res, rej) => {
-      args.forEach((promise, i) => {
-        promise.then(
-          (x) => {
-            newArr[i] = x;
-            count--;
-            if (count <= 0) return res();
-          },
-          (y) => {
-            newArr[i] = y;
-            count--;
-            if (count <= 0) return rej();
-          },
-        );
-      });
-    });
-    newArr = newArr.filter((x) => x !== false);
-    return newArr;
-    
+  async function gougleSearch(q) {
+    const servers = ["web", "image", "video"];
+  
+    const promises = servers.map((server) =>
+      queryServers(server, q).catch(() => `Error fetching from ${server}`),
+    );
+  
+    // Set a timeout for the entire operation
+    const timeoutPromise = new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("timeout")), 80),
+    );
+  
+    try {
+      const results = await Promise.race([Promise.all(promises), timeoutPromise]);
+      return Object.fromEntries(
+        servers.map((server, index) => [server, results[index]]),
+      );
+    } catch (error) {
+        if (error.message === 'timeout') {
+            throw error;
+        }
+       throw error;
+    }
+   
   }
