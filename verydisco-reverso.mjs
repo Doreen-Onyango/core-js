02b2b815489128b5c6e7d02da911bd8fb865c6fa
doonyango <datieno001@gmail.com>
2024-10-22 22:36:03 +0300

    feat: implement discovery reverse

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -2,12 +2,11 @@ import fs from "fs/promises";
 const input = process.argv[2];
 const data = await fs.readFile(input, "utf8")
 
-
-let arr = data.split(' ')
+let arr = data.split(" ")
 let arr1 = []
 for (let i = 0; i <= arr.length -1; i++) {
     let l = arr[i].length
     let le = Math.round(l/2)
     arr1.push(arr[i].slice(le) + arr[i].slice(0, le))
 }
-console.log(arr1.join(' '));
+console.log(arr1.join(" "));
