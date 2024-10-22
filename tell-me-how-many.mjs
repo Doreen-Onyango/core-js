7238f982cfd79542e721ecaf8db073f8d532e6b8
doonyango <datieno001@gmail.com>
2024-10-22 22:48:28 +0300

    feat: implement tell me hoe many

---
+++ tell-me-how-many.mjs
@@ -0,0 +1,9 @@
+import fs from "fs/promises";
+
+let input = process.argv[2]
+if (input === undefined) {
+    input = process.argv[1]
+}
+const data = await fs.readdir(arg, "utf8")
+
+console.log(data.length)
