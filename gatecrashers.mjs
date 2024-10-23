9b9094a06e25c42093a4d3e5213df558a5875050
doonyango <datieno001@gmail.com>
2024-10-23 07:20:52 +0300

    feat: implement server

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -16,3 +16,50 @@ const parse_auth_header = (auth_header) => {
     return { username, password }
 }
 
+const server = http.createServer((req, res) => {
+    const auth = parse_auth_header(req.headers.authorization);
+    if (!auth || !authorized_users[auth.username] || authorized_users[auth.username] !== auth.password) {
+        res.writeHead(401, {
+            'Content-Type': 'application/json',
+            'WWW-Authenticate': 'Basic realm="Authorization Required"'
+        });
+        res.end(JSON.stringify({ error: 'Authorization Required' }))
+        return;
+    }
+    if (req.method === 'POST') {
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk.toString()
+        });
+        req.on('end', async () => {
+            try {
+                if (!body) {
+                    body = {
+                        answer: 'yes',
+                        drink: 'juice',
+                        food: 'pizza'
+                    }
+                }
+
+                const guest_name = req.url.slice(1);
+
+                const guest_dir = path.join(base_dir, 'guests');
+                await fs.mkdir(guest_dir, { recursive: true });
+
+                const file_path = path.join(guest_dir, `${guest_name}.json`);
+                await fs.writeFile(file_path, JSON.stringify(body, null, 2));
+                res.writeHead(200, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify(body))
+
+            } catch (e) {
+                res.writeHead(500, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ error: 'Internal Server Error' }))
+            }
+        })
+    }
+})
+
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
